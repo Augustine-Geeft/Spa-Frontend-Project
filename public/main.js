8436d98ab50d9fb9b6f9ae9e74f9b307954eebd4
Augustine-Geeft <geeftaugustine@gmail.com>
2023-08-16 22:50:27 +0100

    modified index and js files

--- public/main.js
+++ public/main.js
@@ -12,3 +12,31 @@ menu.addEventListener('click', function () {
 	// mobileNav.classList.toggle('visibility');
 })
 
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	margin: 10,
+	nav: true,
+	items: 3,
+	autoplay: true,
+	autoplayTimeout: 4000,
+	autoplayHoverPause: true,
+	responsiveClass: true,
+	responsive: {
+	  0: {
+		items: 1,
+		nav: true,
+		loop: true
+	  },
+	  600: {
+		items: 2,
+		nav: true,
+		loop: true
+	  },
+	  1000: {
+		items: 3,
+		nav: true,
+		loop: true
+	  }
+	}
+  })
+
